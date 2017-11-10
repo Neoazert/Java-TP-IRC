b9f01b939aec0978e88696f7f854f7043a527ffb
MBAPE EYOKE QUINCY p1502985 <p1502985@univ-lyon1.fr>
2017-11-10 09:24:38 +0100

    Classe de base

---
+++ src/java/tp/irc/model/ConnectedClient.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java.tp.irc.model;
+
+/**
+ *
+ * @author p1502985
+ */
+public class ConnectedClient {
+    
+}
